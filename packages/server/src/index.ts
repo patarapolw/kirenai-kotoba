340b7f6654174c8484c7bb223edaf5d3457083b7
Pacharapol Withayasakpunt <polv@polv.cc>
2022-09-18 22:16:59 +0700

    update deps

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,36 +1,42 @@
-import path from 'path'
-import { URL } from 'url'
+import fastify from 'fastify';
+import S from 'jsonschema-definer';
+import path from 'path';
+import { URL } from 'url';
 
-import fastify from 'fastify'
-import fasitfyStatic from 'fastify-static'
-import S from 'jsonschema-definer'
-import pov from 'point-of-view'
+import fasitfyStatic from '@fastify/static';
+import pov from '@fastify/view';
 
-import { Dict, DictElement, initDB } from './db/loki'
-import { expandSmall, reKana, sDictMeaning } from './db/types'
+import { Dict, DictElement, initDB } from './db/loki';
+import { expandSmall, reKana, sDictMeaning } from './db/types';
 
 async function main() {
-  await initDB()
+  await initDB();
 
-  const PORT = process.env['PORT'] || 8080
-  const isDev = process.env['NODE_ENV'] === 'development'
+  const PORT = process.env['PORT'] || 8080;
+  const isDev = process.env['NODE_ENV'] === 'development';
 
-  const app = fastify({
-    logger: {
-      prettyPrint: isDev
-    }
-  })
+  const app = fastify(
+    isDev
+      ? {
+          logger: {
+            transport: {
+              target: 'pino-pretty',
+            },
+          },
+        }
+      : {},
+  );
 
   app.register(fasitfyStatic, {
-    root: path.join(__dirname, '../public')
-  })
+    root: path.join(__dirname, '../public'),
+  });
 
   app.register(pov, {
     engine: {
-      ejs: require('ejs')
+      ejs: require('ejs'),
     },
-    root: path.join(__dirname, '../template')
-  })
+    root: path.join(__dirname, '../template'),
+  });
 
   {
     const sQuery = S.shape({
@@ -43,8 +49,8 @@ async function main() {
       small: S.string().enum('on').optional(),
       format: S.string().enum('txt', 'json'),
       offset: S.integer().minimum(0).optional(),
-      limit: S.integer().minimum(-1).optional()
-    })
+      limit: S.integer().minimum(-1).optional(),
+    });
 
     const sJsonResponse = S.shape({
       meta: S.shape({
@@ -52,30 +58,30 @@ async function main() {
         limit: S.integer(),
         count: S.integer(),
         previous: S.string().optional(),
-        next: S.string().optional()
+        next: S.string().optional(),
       }),
       data: S.list(
         S.shape({
           ja: S.list(S.string()),
           en: S.list(sDictMeaning),
-          tag: S.list(S.string()).optional()
-        }).additionalProperties(true)
-      )
-    })
+          tag: S.list(S.string()).optional(),
+        }).additionalProperties(true),
+      ),
+    });
 
-    const sResponse = S.anyOf(S.string(), sJsonResponse)
+    const sResponse = S.anyOf(S.string(), sJsonResponse);
 
     app.get<{
-      Querystring: typeof sQuery.type
+      Querystring: typeof sQuery.type;
     }>(
       '/generate',
       {
         schema: {
           querystring: sQuery.valueOf(),
           response: {
-            200: sResponse.valueOf()
-          }
-        }
+            200: sResponse.valueOf(),
+          },
+        },
       },
       async (req): Promise<typeof sResponse.type> => {
         const {
@@ -87,90 +93,90 @@ async function main() {
           repeat,
           small,
           format,
-          offset = 0
-        } = req.query
+          offset = 0,
+        } = req.query;
 
-        let { limit = 100 } = req.query
+        let { limit = 100 } = req.query;
 
-        const $and: any[] = []
+        const $and: any[] = [];
 
         const rePattern = pattern.trim()
           ? Array.from(
-              pattern.matchAll(/[\p{sc=Katakana}\p{sc=Hiragana}ー?？]/gu)
+              pattern.matchAll(/[\p{sc=Katakana}\p{sc=Hiragana}ー?？]/gu),
             )
               .join('')
               .replace(/[?？]/g, '.')
-          : ''
+          : '';
 
         if (!rePattern) {
-          $and.push({ length })
+          $and.push({ length });
         }
 
-        const tagSet = new Set(tag ? tag.split(' ') : [])
-        let isCommon = false
+        const tagSet = new Set(tag ? tag.split(' ') : []);
+        let isCommon = false;
         if (tagSet.has('common')) {
-          tagSet.delete('common')
-          isCommon = true
-          $and.push({ primary: true })
+          tagSet.delete('common');
+          isCommon = true;
+          $and.push({ primary: true });
         }
 
         if (exclude) {
-          let chars = Array.from(exclude.matchAll(reKana)).map((m) => m[0]!)
+          let chars = Array.from(exclude.matchAll(reKana)).map((m) => m[0]!);
           if (small) {
-            chars = expandSmall(chars)
+            chars = expandSmall(chars);
           }
 
           $and.push({
             char: {
-              $containsNone: chars
-            }
-          })
+              $containsNone: chars,
+            },
+          });
         }
 
         if (within) {
-          let chars = Array.from(within.matchAll(reKana)).map((m) => m[0]!)
+          let chars = Array.from(within.matchAll(reKana)).map((m) => m[0]!);
           if (small) {
-            chars = expandSmall(chars)
+            chars = expandSmall(chars);
           }
 
           chars.map((c) => {
-            $and.push({ char: { $contains: c } })
-          })
+            $and.push({ char: { $contains: c } });
+          });
         }
 
         if (small) {
-          $and.push({ repeatx: repeat ? { $gt: 0 } : 0 })
+          $and.push({ repeatx: repeat ? { $gt: 0 } : 0 });
         } else {
-          $and.push({ repeat: repeat ? { $gt: 0 } : 0 })
+          $and.push({ repeat: repeat ? { $gt: 0 } : 0 });
         }
 
         if (rePattern) {
-          $and.push({ value: { $regex: new RegExp('^' + rePattern + '$') } })
+          $and.push({ value: { $regex: new RegExp('^' + rePattern + '$') } });
         }
 
         const entries = Dict.chain()
           .find({
             _id: {
-              $in: [...new Set(DictElement.find({ $and }).map((r) => r.dict))]
+              $in: [...new Set(DictElement.find({ $and }).map((r) => r.dict))],
             },
             ...(tagSet.size
               ? { tag: { $containsAny: Array.from(tagSet) } }
-              : {})
+              : {}),
           })
           .simplesort('frequency', { desc: true })
-          .data()
+          .data();
 
-        const count = entries.length
-        limit = limit || -1
+        const count = entries.length;
+        limit = limit || -1;
 
         const makeURL = (offset: number) => {
           const u = new URL(
             req.url,
-            isDev ? 'http://' + req.hostname : 'https://kotoba.polv.cc'
-          )
-          u.searchParams.set('offset', String(offset))
-          return u.href
-        }
+            isDev ? 'http://' + req.hostname : 'https://kotoba.polv.cc',
+          );
+          u.searchParams.set('offset', String(offset));
+          return u.href;
+        };
 
         const out: typeof sJsonResponse.type = {
           meta: {
@@ -182,22 +188,22 @@ async function main() {
             next:
               limit > 0 && offset + limit < count
                 ? makeURL(offset + limit)
-                : undefined
+                : undefined,
           },
           data: entries
             .slice(offset, limit > 0 ? offset + limit : undefined)
             .map((r) => ({
               ja: DictElement.find({
                 dict: r._id,
-                ...(isCommon ? { primary: true } : {})
+                ...(isCommon ? { primary: true } : {}),
               }).map((el) => el.value),
               en: r.meaning,
-              tag: r.tag
-            }))
-        }
+              tag: r.tag,
+            })),
+        };
 
         if (format === 'json') {
-          return out
+          return out;
         }
 
         return out.data
@@ -205,20 +211,20 @@ async function main() {
             (d) =>
               `${d.ja.join(' ')} - ${d.en
                 .map((m) => m.gloss.join(', '))
-                .join(' / ')}`
+                .join(' / ')}`,
           )
-          .join('\n')
-      }
-    )
+          .join('\n');
+      },
+    );
   }
 
   app.get('/', (_, reply) => {
-    reply.view('index')
-  })
+    reply.view('index');
+  });
 
-  app.listen(PORT, isDev ? '' : '0.0.0.0')
+  app.listen({ port: Number(PORT), host: isDev ? '' : '0.0.0.0' });
 }
 
 if (require.main === module) {
-  main()
+  main();
 }
